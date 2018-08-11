4c37dd9ca9c23dae5ea6e67615d28e70a018240c
Jorge Moller <jmoller2106@gmail.com>
2018-08-11 14:10:10 -0300

    Add comment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./containers/App/App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import "antd/dist/antd.css";
-
+// Just adding a comment here
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
